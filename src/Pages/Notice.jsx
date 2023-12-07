b3a9a41866678147e850aec907027f56c400a447
Atif Diwan <atifdiwan8@gmail.com>
2023-12-07 15:55:58 +0530

    Update Notice.jsx

---
+++ src/Pages/Notice.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import NoticeComponent from "../components/NoticeComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebaseConfig";
+import Loader from "../components/common/Loader";
+
+export default function Notice({ currentUser }) {
+  const [loading, setLoading] = useState(true);
+  let navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      if (!res?.accessToken) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+  return loading ?( 
+  <Loader /> 
+  ):( 
+  <NoticeComponent currentUser={currentUser} />
+  );
+}
