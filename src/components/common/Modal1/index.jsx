97ee99e699c70792c48bab95de3ed93e8277e38c
Atif Diwan <atifdiwan8@gmail.com>
2023-12-07 15:55:34 +0530

    Create index.jsx

---
+++ src/components/common/Modal1/index.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Button, Modal, Progress } from "antd";
+import { AiOutlinePicture } from "react-icons/ai";
+import ReactQuill from "react-quill";
+import "./index.scss";
+
+const ModalComponent = ({
+  modalOpen,
+  setModalOpen,
+  sendStatus,
+  setStatus,
+  status,
+  isEdit,
+  updateStatus,
+  uploadNoticeImage,
+  setPostImage,
+  postImage,
+  currentPost,
+  setCurrentPost,
+}) => {
+  const [progress, setProgress] = useState(0);
+  return (
+    <>
+      <Modal
+        title="Upload a Notice"
+        centered
+        open={modalOpen}
+        onOk={() => {
+          setStatus("");
+          setModalOpen(false);
+          setPostImage("");
+          setCurrentPost({});
+        }}
+        onCancel={() => {
+          setStatus("");
+          setModalOpen(false);
+          setPostImage("");
+          setCurrentPost({});
+        }}
+        footer={[
+          <Button
+            onClick={isEdit ? updateStatus : sendStatus}
+            key="submit"
+            type="primary"
+            disabled={status.length > 0 ? false : true}
+          >
+            {isEdit ? "Update" : "Post"}
+          </Button>,
+        ]}
+      >
+        <div className="posts-body">
+          <ReactQuill
+            className="modal-input"
+            theme="snow"
+            value={status}
+            placeholder="Type Here..."
+            onChange={setStatus}
+          />
+          {progress === 0 || progress === 100 ? (
+            <></>
+          ) : (
+            <div className="progress-bar">
+              <Progress type="circle" percent={progress} />
+            </div>
+          )}
+          {postImage?.length > 0 || currentPost?.postImage?.length ? (
+            <img
+              className="preview-image"
+              src={postImage || currentPost?.postImage}
+              alt="postImage"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+        <label for="pic-upload">
+          <AiOutlinePicture size={35} className="picture-icon" />
+        </label>
+        <input
+          id="pic-upload"
+          type={"file"}
+          hidden
+          onChange={(event) => uploadNoticeImage(event.target.files[0], setNoticeImage, setProgress)
+          }
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent;
